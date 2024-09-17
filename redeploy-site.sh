6b436e1854c379e0ec784c3214d8ac3194c78c48
satoshi-sh <satoss1108@gmail.com>
2024-09-17 18:02:54 -0500

    Removed chmod line. It's not working as expected

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 cd ~/projects/homelab-portfolio
-chmod u+x redeploy-site.sh
 git fetch && git reset origin/main --hard
 docker compose -f docker-compose.prod.yml down
 docker compose -f docker-compose.prod.yml up -d --build
