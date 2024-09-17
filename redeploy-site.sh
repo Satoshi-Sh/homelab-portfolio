67e81d3b3fd5c00498b97dfe59c6dc15e8925c8a
satoshi-sh <satoss1108@gmail.com>
2024-09-17 17:50:09 -0500

    Add chmod line for redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd ~/projects/homelab-portfolio
+chmod u+x redeploy-site.sh
 git fetch && git reset origin/main --hard
 docker compose -f docker-compose.prod.yml down
 docker compose -f docker-compose.prod.yml up -d --build
