691fd50e08fcaff1e0adab1c87384df2bcb53dee
satoshi-sh <satoss1108@gmail.com>
2024-09-24 10:28:56 -0500

    Test running script inside the project folder

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd ~/projects/homelab-portfolio
 git fetch && git reset origin/main --hard
+chmod +x redeploy-site.sh
 docker compose -f docker-compose.prod.yml down
 docker compose -f docker-compose.prod.yml up -d --build
