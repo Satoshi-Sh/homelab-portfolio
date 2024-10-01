fb981c8adecb5339e498c1f1a6a5e79214ffd582
satoshi-sh <satoss1108@gmail.com>
2024-10-01 08:27:33 -0500

    Add a command to remove old images

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -3,4 +3,5 @@ cd ~/projects/homelab-portfolio
 git fetch && git reset origin/main --hard
 chmod +x redeploy-site.sh
 docker compose -f docker-compose.prod.yml down
+docker image prune -f
 docker compose -f docker-compose.prod.yml up -d --build
